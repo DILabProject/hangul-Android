b2aa37a720e5aed53a94be8fa50e088f04f7dee4
GeonYoungKim <c2619zz@naver.com>
2018-01-03 17:35:45 +0900

    drag listener 입힌 중간 단계 커밋

--- app/src/main/java/com/example/namgiwon/hangul/PaintText.java
+++ app/src/main/java/com/example/namgiwon/hangul/PaintText.java
@@ -1,5 +1,8 @@
 package com.example.namgiwon.hangul;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,9 +22,12 @@ import java.util.HashMap;
  * Created by namgiwon on 2017. 12. 29..
  */
 
-public class PaintText extends AppCompatActivity {
+public class PaintText extends AppCompatActivity implements View.OnTouchListener,View.OnDragListener {
     RelativeLayout parentLayout;
     String text;
+    Path path  = new Path();
+    private static final String IMAGEVIEW_TAG = "드래그 이미지";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +39,13 @@ public class PaintText extends AppCompatActivity {
 
         for(int i = 0 ; i < 23; i++){
             ImageView iv =  new ImageView(this);
+            iv.setClickable(true);
+            iv.setTag(IMAGEVIEW_TAG);
 
             if(i > 10){
                 iv.setId(i);
-                iv.setOnTouchListener(tListener);
+                iv.setOnTouchListener(this);
+                iv.setOnDragListener(this);
                 iv.setBackgroundResource(R.drawable.b);
                 iv.setLayoutParams(new RelativeLayout.LayoutParams(100, 50));
                 ViewGroup.MarginLayoutParams margin = new ViewGroup.MarginLayoutParams(
@@ -47,7 +56,8 @@ public class PaintText extends AppCompatActivity {
 
             else {
                 iv.setId(i);
-                iv.setOnDragListener(dListener);
+                iv.setOnTouchListener(this);
+                iv.setOnDragListener(this);
                 iv.setBackgroundResource(R.drawable.a);
                 iv.setLayoutParams(new RelativeLayout.LayoutParams(50, 100));
                 // margin 설정을 담을 param을 하나 만들어서 ImageView에 적용합니다.
@@ -58,27 +68,84 @@ public class PaintText extends AppCompatActivity {
             }
             parentLayout.addView(iv);
         }
+    }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+            float x = motionEvent.getX();
+            float y = motionEvent.getY();
 
+            switch (motionEvent.getAction()){
+                case MotionEvent.ACTION_DOWN:
+
+                    Log.d("id down == ", String.valueOf(view.getId()));
+                    Toast.makeText(getApplicationContext(),view.getId()+"",Toast.LENGTH_SHORT).show();
+                    ClipData.Item item = new ClipData.Item(
+                            (CharSequence) view.getTag());
+
+                    String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
+                    ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
+                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                            view);
+                    view.startDrag(data, // data to be dragged
+                            shadowBuilder, // drag shadow
+                            view, // 드래그 드랍할  Vew
+                            0 // 필요없은 플래그
+                    );
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    path.moveTo(x, y);
+                    Log.d("id move == ", String.valueOf(view.getId()));
+                    Toast.makeText(getApplicationContext(),view.getId()+"",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+    }
+
+    @Override
+    public boolean onDrag(View view, DragEvent dragEvent) {
+        switch (dragEvent.getAction()) {
+
+            // 이미지를 드래그 시작될때
+            case DragEvent.ACTION_DRAG_STARTED:
+                Log.d("DragClickListener", "ACTION_DRAG_STARTED");
+                break;
+
+            // 드래그한 이미지를 옮길려는 지역으로 들어왔을때
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.d("id move == ", String.valueOf(view.getId()));
+                // 이미지가 들어왔다는 것을 알려주기 위해 배경이미지 변경
+                Toast.makeText(getApplicationContext(),view.getId()+"",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
     }
 
 
-    private View.OnTouchListener tListener = new View.OnTouchListener() {
+    /*private View.OnTouchListener tListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+
             switch (event.getAction()){
                 case MotionEvent.ACTION_DOWN:
-                    Toast.makeText(getApplicationContext(),String.valueOf(view.getId()),Toast.LENGTH_LONG).show();
+                    path.moveTo(x, y);
+                    Log.d("id down == ", String.valueOf(view.getId()));
+                    Toast.makeText(getApplicationContext(),view.getId()+"",Toast.LENGTH_SHORT).show();
+                    break;
                 case MotionEvent.ACTION_MOVE:
-                    Log.d("id == ", String.valueOf(view.getId()));
-                    Toast.makeText(getApplicationContext(),String.valueOf(view.getId()),Toast.LENGTH_LONG).show();
+                    path.moveTo(x, y);
+                    Log.d("id move == ", String.valueOf(view.getId()));
+                    Toast.makeText(getApplicationContext(),view.getId()+"",Toast.LENGTH_SHORT).show();
                     break;
                 
 
             }
+            return false;
             //Toast.makeText(getApplicationContext(),"aa",Toast.LENGTH_LONG).show();
 
-            return true;
         }
     };
 
@@ -96,6 +163,6 @@ public class PaintText extends AppCompatActivity {
             }
             return true;
         }
-    };
+    };*/
 
 }
